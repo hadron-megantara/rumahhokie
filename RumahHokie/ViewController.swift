2dbed6e59052405a03931e11cb1b3d36d960d358
Hadron Megantara <hadron@macs-macbook-pro.local>
2018-09-11 09:46:45 +0700

    Initial Commit

---
+++ RumahHokie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RumahHokie
+//
+//  Created by Hadron Megantara on 11/09/18.
+//  Copyright © 2018 Hadron Megantara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
