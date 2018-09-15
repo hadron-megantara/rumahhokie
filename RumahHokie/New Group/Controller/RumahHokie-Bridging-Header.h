dd9c0098d86708d5105b7988362c26a180d1541c
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-15 14:17:27 +0700

    Add Bridging Header

---
+++ RumahHokie/New Group/Controller/RumahHokie-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  RumahHokie-Bridging-Header.h
+//  RumahHokie
+//
+//  Created by Hadron Megantara on 15/09/18.
+//  Copyright © 2018 Hadron Megantara. All rights reserved.
+//
+
+#ifndef RumahHokie_Bridging_Header_h
+#define RumahHokie_Bridging_Header_h
+
+
+#endif /* RumahHokie_Bridging_Header_h */
