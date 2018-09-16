57582fbc18ce6185e7fd195e3068bb22c56898aa
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-16 19:11:17 +0700

    News Page

--- RumahHokie/NewsController.swift
+++ RumahHokie/NewsController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class NewsController: UIViewController {
     @IBOutlet weak var bottomMenu: UIView!
+    @IBOutlet weak var btnProperty: UIButton!
+    @IBOutlet weak var btnNew: UIButton!
+    @IBOutlet weak var btnPopular: UIButton!
+    @IBOutlet weak var btnGalery: UIButton!
+    @IBOutlet weak var btnVideo: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController?.navigationBar.isHidden = true;
+        btnProperty.titleLabel?.adjustsFontSizeToFitWidth = true
+        btnNew.titleLabel?.adjustsFontSizeToFitWidth = true
+        btnPopular.titleLabel?.adjustsFontSizeToFitWidth = true
+        btnGalery.titleLabel?.adjustsFontSizeToFitWidth = true
+        btnVideo.titleLabel?.adjustsFontSizeToFitWidth = true
+        
+        self.navigationController?.navigationBar.isHidden = true
         let bottomMenuView = Bundle.main.loadNibNamed("BottomMenu", owner: nil, options: nil)![0] as! UIView
         bottomMenu.addSubview(bottomMenuView)
     }
