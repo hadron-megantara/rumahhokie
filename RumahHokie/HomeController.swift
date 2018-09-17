f75d975f72bd846e4c07e06d73a47058cc965c3c
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-17 12:22:44 +0700

    top banner

--- RumahHokie/HomeController.swift
+++ RumahHokie/HomeController.swift
@@ -10,13 +10,45 @@ import UIKit
 
 class HomeController: UIViewController {
     @IBOutlet weak var bottomMenu: UIView!
+    @IBOutlet weak var carouselView: AACarousel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let res = loadTopBanner()
+        print(res)
+        
         self.navigationController?.navigationBar.isHidden = true;
         let bottomMenuView = Bundle.main.loadNibNamed("BottomMenu", owner: nil, options: nil)![0] as! UIView
         bottomMenu.addSubview(bottomMenuView)
+    }
+    
+    func loadTopBanner(){
+        let manager = AFHTTPSessionManager(baseURL: URL(string: "http://rumahhokie.com"))
+        
+        let params = [
+            "categories": "258",
+            "offset": "1",
+            "per_page": "20",
+            "_embed": "1"
+        ]
+        
+        manager.get(
+            "/beritaproperti/wp-json/wp/v2/posts",
+            parameters: params,
+            success:
+            {
+                (task: URLSessionDataTask!, res: Any) in
+                print(res)
+            },
+            failure:
+            {
+                (operation, error) in
+                print("Error: " + error.localizedDescription)
+            }
+        )
+        
+        return manager
     }
     
     override func didReceiveMemoryWarning() {
