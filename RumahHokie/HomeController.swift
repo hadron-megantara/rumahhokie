afa7310b610a8cf654b99bd15f7c390d54fa33b5
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-18 10:25:41 +0700

    ImageSlider - 1

--- RumahHokie/HomeController.swift
+++ RumahHokie/HomeController.swift
@@ -9,9 +9,11 @@
 import UIKit
 import AACarousel
 
-class HomeController: UIViewController {
+class HomeController: UIViewController, AACarouselDelegate {
     @IBOutlet weak var bottomMenu: UIView!
     @IBOutlet weak var carouselView: AACarousel!
+    
+    var titleArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +32,7 @@ class HomeController: UIViewController {
             "schedule": "now"
         ]
         
-//        var url: String = ""
+        var url: String = ""
         
         manager.get(
             "/prm_banner_top",
@@ -43,11 +45,10 @@ class HomeController: UIViewController {
                     if let detail = res["prm_banner_top"]{
                         if let d = detail as? Array<AnyObject>{
                             for array in d{
-                                let name = array.value(forKey: "prm_top_image") as! String
-                                print(name)
+                                let resImage = array.value(forKey: "prm_top_image") as! String
+                                let resUrl = array.value(forKey: "prm_top_url") as! String
                                 
-//                                let b = array["prm_top_url"] + array["prm_top_image"]
-//                                url += array["prm_top_url"] + array["prm_top_image"] + ","
+                                url += resUrl + resImage + ","
                             }
                         }
                     }
@@ -60,19 +61,32 @@ class HomeController: UIViewController {
             }
         )
         
-//        let pathArray = []
-//        titleArray = ["picture 1","picture 2","picture 3","picture 4","picture 5"]
-//        carouselView.delegate = self
-//        carouselView.setCarouselData(paths: pathArray,  describedTitle: titleArray, isAutoScroll: true, timer: 5.0, defaultImage: "defaultImage")
-//        //optional methods
-//        carouselView.setCarouselOpaque(layer: false, describedTitle: false, pageIndicator: false)
-//        carouselView.setCarouselLayout(displayStyle: 0, pageIndicatorPositon: 5, pageIndicatorColor: nil, describedTitleColor: nil, layerColor: nil)
+        let pathArray = [url]
+        titleArray = ["picture 1","picture 2","picture 3","picture 4","picture 5"]
+        carouselView.delegate = self
+        carouselView.setCarouselData(paths: pathArray,  describedTitle: titleArray, isAutoScroll: true, timer: 5.0, defaultImage: "defaultImage")
+        //optional methods
+        carouselView.setCarouselOpaque(layer: false, describedTitle: false, pageIndicator: false)
+        carouselView.setCarouselLayout(displayStyle: 0, pageIndicatorPositon: 5, pageIndicatorColor: nil, describedTitleColor: nil, layerColor: nil)
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func didSelectCarouselView(_ view: AACarousel, _ index: Int) {
+        let alert = UIAlertView.init(title:"Alert" , message: titleArray[index], delegate: self, cancelButtonTitle: "OK")
+        alert.show()
+    }
+    
+    func callBackFirstDisplayView(_ imageView: UIImageView, _ url: [String], _ index: Int) {
+        
+    }
+    
+    func downloadImages(_ url: String, _ index: Int) {
+        
     }
     
     
