87ea786354faf1191cd2acfccfd519299582a14c
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-19 01:59:09 +0700

    PropertyList Scene

---
+++ RumahHokie/PropertyListController.swift
@@ -0,0 +1,25 @@
+//
+//  PropertyListController.swift
+//  RumahHokie
+//
+//  Created by Hadron Megantara on 19/09/18.
+//  Copyright © 2018 Hadron Megantara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PropertyListController: UIViewController {
+    @IBOutlet weak var bottomMenu: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
