b277fa3ba55bbdb848c1a41902e670c557505993
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-18 22:57:45 +0700

    template MidBanner

--- RumahHokie/HomeController.swift
+++ RumahHokie/HomeController.swift
@@ -14,6 +14,7 @@ import Kingfisher
 class HomeController: UIViewController, AACarouselDelegate {
     @IBOutlet weak var bottomMenu: UIView!
     @IBOutlet weak var carouselView: AACarousel!
+    @IBOutlet var bannerMid: [UIView]!
     
     var url = [String]()
     var titleArray = [String]()
