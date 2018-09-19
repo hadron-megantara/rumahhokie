d1eafada44ae875b70556fb9d6536b6957e0e30f
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-09-20 00:01:31 +0700

    LoginRegister

---
+++ RumahHokie/LoginController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginController.swift
+//  RumahHokie
+//
+//  Created by Hadron Megantara on 19/09/18.
+//  Copyright © 2018 Hadron Megantara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginController: UIViewController, UITextFieldDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
