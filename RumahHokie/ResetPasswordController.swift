a55e77f3d64fdcbda9d1aaf534a7f4835a40499c
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-10-12 02:03:39 +0700

    ResetPassword View

---
+++ RumahHokie/ResetPasswordController.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordController.swift
+//  RumahHokie
+//
+//  Created by Hadron Megantara on 12/10/18.
+//  Copyright © 2018 Hadron Megantara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ResetPasswordController: UIViewController, UITextFieldDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
