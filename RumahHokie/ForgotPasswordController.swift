7e835852ad83ade0255e406238964857d9833e63
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-10-22 17:48:33 +0700

    Add ResetPassword

---
+++ RumahHokie/ForgotPasswordController.swift
@@ -0,0 +1,25 @@
+//
+//  ForgotPasswordController.swift
+//  RumahHokie
+//
+//  Created by Hadron Megantara on 22/10/18.
+//  Copyright © 2018 Hadron Megantara. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ForgotPasswordController: UIViewController, UITextFieldDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true;
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
