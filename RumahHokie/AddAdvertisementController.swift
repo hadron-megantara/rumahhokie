61eabbda4148f69daf9ed6dcd5cf234866e1938d
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-10-23 11:53:46 +0700

    fix crash

--- RumahHokie/AddAdvertisementController.swift
+++ RumahHokie/AddAdvertisementController.swift
@@ -29,13 +29,23 @@ class AddAdvertisementController: UIViewController, UITextFieldDelegate, UIPicke
     var propertyCity: Int = 1
     var propertyCityString: String = ""
     
+    var propertyArea: Int = 1
+    var propertyAreaString: String = ""
+    
     var propertyStatus: Int = 0
     var propertyTypeData = ["Apartemen", "Rumah", "Tanah", "Komersial", "Properti Baru"]
     var propertyProvinceData: Array = [Any]()
-    var pickerPropertyType = UIPickerView()
     var pickerPropertyProvince = UIPickerView()
+    var propertyCityData: Array = [Any]()
+    var pickerPropertyCity = UIPickerView()
+    var propertyAreaData: Array = [Any]()
+    var pickerPropertyArea = UIPickerView()
+    var pickerPropertyType = UIPickerView()
+    
     let alert = UIAlertController(title: "", message: "\n\n\n\n\n\n\n\n\n\n", preferredStyle: .alert)
     let alert2 = UIAlertController(title: "", message: "\n\n\n\n\n\n\n\n\n\n", preferredStyle: .alert)
+    let alert3 = UIAlertController(title: "", message: "\n\n\n\n\n\n\n\n\n\n", preferredStyle: .alert)
+    let alert4 = UIAlertController(title: "", message: "\n\n\n\n\n\n\n\n\n\n", preferredStyle: .alert)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,8 +81,32 @@ class AddAdvertisementController: UIViewController, UITextFieldDelegate, UIPicke
         pickerPropertyProvince.dataSource = self
         pickerPropertyProvince.isHidden = true
         
+        var pickerRect3 = pickerPropertyCity.frame
+        pickerRect3.origin.x = -30
+        pickerRect3.origin.y = 0
+        pickerPropertyCity.tag = 3
+        pickerPropertyCity.frame = pickerRect3
+        pickerPropertyCity.delegate = self
+        pickerPropertyCity.dataSource = self
+        pickerPropertyCity.isHidden = true
+        
+        var pickerRect4 = pickerPropertyArea.frame
+        pickerRect4.origin.x = -30
+        pickerRect4.origin.y = 0
+        pickerPropertyArea.tag = 4
+        pickerPropertyArea.frame = pickerRect4
+        pickerPropertyArea.delegate = self
+        pickerPropertyArea.dataSource = self
+        pickerPropertyArea.isHidden = true
+        
         alert2.isModalInPopover = true
         alert2.view.addSubview(pickerPropertyProvince)
+        
+        alert3.isModalInPopover = true
+        alert3.view.addSubview(pickerPropertyCity)
+        
+        alert4.isModalInPopover = true
+        alert4.view.addSubview(pickerPropertyArea)
     }
     
     func loadProvince(){
@@ -108,19 +142,47 @@ class AddAdvertisementController: UIViewController, UITextFieldDelegate, UIPicke
         group.enter()
         
         DispatchQueue.main.async {
-            Alamofire.request("http://api.rumahhokie.com/mstr_provinsi/?order_by=mstr_provinsi_desc&order_type=asc", method: .get).responseJSON { response in
+            Alamofire.request("http://api.rumahhokie.com/mstr_provinsi/\(self.propertyProvince)/mstr_kota?order_by=mstr_kota_desc&order_type=asc", method: .get).responseJSON { response in
                 
                 if let json = response.result.value {
-                    if let res = (json as AnyObject).value(forKey: "mstr_provinsi"){
+                    if let res = (json as AnyObject).value(forKey: "mstr_kota"){
                         if let resArray = res as? Array<AnyObject>{
                             for r in resArray{
-                                self.propertyProvinceData.append(r)
+                                self.propertyCityData.append(r)
                             }
                         }
                     }
                 }
                 
-                self.pickerPropertyProvince.delegate = self;
+                self.pickerPropertyCity.delegate = self;
+            }
+            
+            group.leave()
+        }
+        
+        group.notify(queue: DispatchQueue.main) {
+            
+        }
+    }
+    
+    func loadArea(){
+        let group = DispatchGroup()
+        group.enter()
+        
+        DispatchQueue.main.async {
+            Alamofire.request("http://api.rumahhokie.com/mstr_provinsi/\(self.propertyProvince)/mstr_kota/\(self.propertyCity)/mstr_wilayah?order_by=mstr_wilayah_desc&order_type=asc", method: .get).responseJSON { response in
+                
+                if let json = response.result.value {
+                    if let res = (json as AnyObject).value(forKey: "mstr_wilayah"){
+                        if let resArray = res as? Array<AnyObject>{
+                            for r in resArray{
+                                self.propertyAreaData.append(r)
+                            }
+                        }
+                    }
+                }
+                
+                self.pickerPropertyArea.delegate = self;
             }
             
             group.leave()
@@ -182,11 +244,31 @@ class AddAdvertisementController: UIViewController, UITextFieldDelegate, UIPicke
     }
     
     @IBAction func propertyCityAction(_ sender: Any) {
+        pickerPropertyCity.isHidden = false
+        
+        self.present(alert3, animated: true, completion:{
+            self.alert3.view.superview?.isUserInteractionEnabled = true
+            self.alert3.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.alertController3BackgroundTapped)))
+        })
+    }
     
+    @objc func alertController3BackgroundTapped(){
+        pickerPropertyCity.isHidden = true
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func propertyAreaAction(_ sender: Any) {
+        pickerPropertyArea.isHidden = false
         
+        self.present(alert4, animated: true, completion:{
+            self.alert4.view.superview?.isUserInteractionEnabled = true
+            self.alert4.view.superview?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.alertController4BackgroundTapped)))
+        })
+    }
+    
+    @objc func alertController4BackgroundTapped(){
+        pickerPropertyArea.isHidden = true
+        self.dismiss(animated: true, completion: nil)
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -196,20 +278,34 @@ class AddAdvertisementController: UIViewController, UITextFieldDelegate, UIPicke
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         if pickerView.tag == 1{
             return propertyTypeData.count
-        } else{
+        } else if pickerView.tag == 2{
             return propertyProvinceData.count
+        } else if pickerView.tag == 3{
+            return propertyCityData.count
         }
+        
+        return propertyAreaData.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if pickerView.tag == 1{
             return propertyTypeData[row]
-        } else{
+        } else if pickerView.tag == 2{
             let dataProvinceObj = propertyProvinceData[row] as AnyObject
             let dataProvince = dataProvinceObj.value(forKey: "mstr_provinsi_desc")
             
             return dataProvince as? String
+        } else if pickerView.tag == 3{
+            let dataCityObj = propertyCityData[row] as AnyObject
+            let dataCity = dataCityObj.value(forKey: "mstr_kota_desc")
+            
+            return dataCity as? String
         }
+        
+        let dataAreaObj = propertyAreaData[row] as AnyObject
+        let dataArea = dataAreaObj.value(forKey: "mstr_wilayah_desc")
+        
+        return dataArea as? String
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
@@ -244,6 +340,30 @@ class AddAdvertisementController: UIViewController, UITextFieldDelegate, UIPicke
             
             pickerPropertyProvince.isHidden = true
             alert2.dismiss(animated: true, completion: nil)
+        } else if pickerView.tag == 3{
+            let dataCityObj = propertyCityData[row] as AnyObject
+            let dataCityString = dataCityObj.value(forKey: "mstr_kota_desc")
+            let dataCityId = dataCityObj.value(forKey: "mstr_kota_id")
+            
+            loadArea()
+            
+            propertyCityString = dataCityString as! String
+            propertyCity = dataCityId as! Int
+            btnPropertyCity.setTitle(propertyCityString, for: UIControlState.normal)
+            
+            pickerPropertyCity.isHidden = true
+            alert3.dismiss(animated: true, completion: nil)
+        }  else if pickerView.tag == 4{
+            let dataAreaObj = propertyAreaData[row] as AnyObject
+            let dataAreaString = dataAreaObj.value(forKey: "mstr_wilayah_desc")
+            let dataAreaId = dataAreaObj.value(forKey: "mstr_wilayah_id")
+            
+            propertyAreaString = dataAreaString as! String
+            propertyArea = dataAreaId as! Int
+            btnPropertyArea.setTitle(propertyAreaString, for: UIControlState.normal)
+            
+            pickerPropertyArea.isHidden = true
+            alert4.dismiss(animated: true, completion: nil)
         }
     }
 }
