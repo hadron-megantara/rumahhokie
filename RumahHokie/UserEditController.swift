155991057161e72f25653d600c82fe28862db9b5
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-11-16 11:49:24 +0700

    Progress-AccountEdit

--- RumahHokie/UserEditController.swift
+++ RumahHokie/UserEditController.swift
@@ -9,6 +9,13 @@ import Foundation
 import UIKit
 
 class UserEditController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var txtFullName: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var accountTypePicker: UIPickerView!
+    @IBOutlet weak var constraintPhotoHeight: NSLayoutConstraint!
+    @IBOutlet weak var imgPhoto: UIImageView!
+    @IBOutlet weak var btnChoosePhoto: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,4 +32,16 @@ class UserEditController: UIViewController, UITextFieldDelegate {
         navigationController?.popToRootViewController(animated: true)
     }
     
+    @IBAction func choosePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            imagePicker.sourceType = .savedPhotosAlbum;
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func saveAction(_ sender: Any) {
+        
+    }
 }
